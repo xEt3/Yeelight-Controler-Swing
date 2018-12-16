1133d07800e253e69531ec5726695c76e9dde35d
Nacho <belmonteperona@gmail.com>
2018-12-16 22:56:22 +0100

    aparentemente todo funciona, exporto a jar.

--- ControlYeelightSwing/src/Panel.java
+++ ControlYeelightSwing/src/Panel.java
@@ -30,9 +30,10 @@ import java.awt.Color;
 public class Panel {
 
 	public JFrame frame;
-
+	public Color colorPrincipal = null;
 	public YeelightDevice device = null;
-	
+	//int Intensidad = 0;
+
 	/**
 	 * Launch the application.
 	 */
@@ -63,56 +64,56 @@ public class Panel {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 778, 450);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		/**
 		 * Panel de Ip bombilla
 		 */
 		JPanel panel_2 = new JPanel();
 		frame.getContentPane().add(panel_2, BorderLayout.NORTH);
 		panel_2.setLayout(new GridLayout(1, 0, 0, 0));
-		
+
 		JTextPane txtIpBombilla = new JTextPane();
 		txtIpBombilla.setEditable(false);
 		txtIpBombilla.setText("IP Bombilla");
 		panel_2.add(txtIpBombilla);
-		
+
 		JEditorPane ipAdd = new JEditorPane();
 		panel_2.add(ipAdd);
-		
+		ipAdd.setText("192.168.100.44");
+
 		JButton btnConectar = new JButton("Conectar");
 		btnConectar.setBackground(new Color(255, 0, 0));
 		panel_2.add(btnConectar);
-		
+
 		btnConectar.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				try {
 					// Instantiate your device (with its IP)
 					device = new YeelightDevice(ipAdd.getText());
 					btnConectar.setBackground(Color.green);
-					
-				} catch (YeelightSocketException e1) {
+
+				} catch (Exception e1) {
 					System.out.println(ipAdd.getText());
 					btnConectar.setBackground(Color.RED);
 				}
-				
 			}
 		});
-		
+
 		/**
 		 * Panel de encendido apagado
 		 */
 		JPanel panel = new JPanel();
 		frame.getContentPane().add(panel, BorderLayout.WEST);
 		panel.setLayout(new GridLayout(0, 1, 0, 0));
-		
+
 		// Encender
 		JButton btnOn = new JButton("ON");
 		btnOn.setBackground(new Color(0, 255, 0));
 		panel.add(btnOn);
-		 btnOn.addActionListener(new ActionListener() {
-			
+		btnOn.addActionListener(new ActionListener() {
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				try {
@@ -122,14 +123,14 @@ public class Panel {
 				}
 			}
 		});
-		
-		 // Apagar
+
+		// Apagar
 		JButton btnOff = new JButton("OFF");
 		btnOff.setBackground(new Color(255, 0, 0));
 		panel.add(btnOff);
-		
+
 		btnOff.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				try {
@@ -139,94 +140,99 @@ public class Panel {
 				}
 			}
 		});
-		
-		
-		// Intensidad de brillo
-		
+
+		/**
+		 * Intensidad brillo
+		 */
+
 		JSlider sliderIntensidad = new JSlider(JSlider.HORIZONTAL, 0, 100, 50);
-		
+
 		sliderIntensidad.addChangeListener(new ChangeListener() {
-			
+
 			@Override
 			public void stateChanged(ChangeEvent e) {
-				try {
-					device.setBrightness(sliderIntensidad.getValue());
-				} catch (Exception k) {
-					btnConectar.setBackground(Color.red);
-				}
-				
+				new Thread(new Runnable() {
+
+					@Override
+					public void run() {
+						if (!sliderIntensidad.getValueIsAdjusting()) {
+						try {
+							device.setBrightness(sliderIntensidad.getValue());
+							//Intensidad = sliderIntensidad.getValue();
+						} catch (Exception k) {
+							System.out.println(sliderIntensidad.getValue());
+							//Intensidad = sliderIntensidad.getValue();
+						}
+
+					}}
+				}).start();
+
 			}
 		});
 		frame.getContentPane().add(sliderIntensidad, BorderLayout.SOUTH);
-		
-		
+
 		/**
 		 * Panel Colores
 		 */
-		
+
 		ActionListener cambiarColor = new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				JButton btnEvaluar = (JButton) e.getSource();
-				
+
 				try {
 
-					device.setRGB(btnEvaluar.getBackground().getRed(),btnEvaluar.getBackground().getGreen(),btnEvaluar.getBackground().getBlue());
+					device.setRGB(btnEvaluar.getBackground().getRed(), btnEvaluar.getBackground().getGreen(),
+							btnEvaluar.getBackground().getBlue());
 
 				} catch (Exception k) {
 
 					btnConectar.setBackground(Color.red);
 				}
-				
+
 			}
 		};
-		
+
 		JPanel panel_1 = new JPanel();
 		panel_1.setBackground(new Color(0, 0, 0));
 		frame.getContentPane().add(panel_1, BorderLayout.CENTER);
-		
+
 		JButton btnColor = new JButton("");
 		btnColor.setForeground(Color.BLACK);
-		btnColor.setSelectedIcon(new ImageIcon("/home/nacho/Escritorio/bombillaka.png"));
-		btnColor.setIcon(new ImageIcon("/home/nacho/Escritorio/bombillaka.png"));
+		btnColor.setSelectedIcon(new ImageIcon("resources/bombillaka.png"));
+		btnColor.setIcon(new ImageIcon("resources/bombillaka.png"));
 		btnColor.setBackground(Color.BLACK);
 		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
-		gl_panel_1.setHorizontalGroup(
-			gl_panel_1.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_1.createSequentialGroup()
-					.addGap(237)
-					.addComponent(btnColor, GroupLayout.PREFERRED_SIZE, 221, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(258, Short.MAX_VALUE))
-		);
-		gl_panel_1.setVerticalGroup(
-			gl_panel_1.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_1.createSequentialGroup()
-					.addGap(67)
-					.addComponent(btnColor, GroupLayout.PREFERRED_SIZE, 221, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(90, Short.MAX_VALUE))
-		);
+		gl_panel_1.setHorizontalGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_1.createSequentialGroup().addGap(237)
+						.addComponent(btnColor, GroupLayout.PREFERRED_SIZE, 221, GroupLayout.PREFERRED_SIZE)
+						.addContainerGap(258, Short.MAX_VALUE)));
+		gl_panel_1.setVerticalGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_1.createSequentialGroup().addGap(67)
+						.addComponent(btnColor, GroupLayout.PREFERRED_SIZE, 221, GroupLayout.PREFERRED_SIZE)
+						.addContainerGap(90, Short.MAX_VALUE)));
 		panel_1.setLayout(gl_panel_1);
 		btnColor.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				Color colorSeleccionado = JColorChooser.showDialog(new JColorChooser(), "Elege color", Color.RED);
-				
+				Color colorSeleccionado = JColorChooser.showDialog(new JColorChooser(), "Elige color", Color.RED);
+
 				btnColor.setBackground(colorSeleccionado);
-				
+
 				try {
 
-					device.setRGB(colorSeleccionado.getRed(), colorSeleccionado.getGreen(), colorSeleccionado.getBlue());
+					device.setRGB(colorSeleccionado.getRed(), colorSeleccionado.getGreen(),
+							colorSeleccionado.getBlue());
 
 				} catch (Exception e) {
 
 					btnConectar.setBackground(Color.red);
 				}
-				
+
 			}
 		});
-
 
 	}
 
